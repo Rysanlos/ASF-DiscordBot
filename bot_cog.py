bceaa32b13e97326a2c530761c7e7d6275c871e0
LeO-Fr <leo.fr_trackmania@hotmail.fr>
2018-05-29 14:25:54 -0700

    spaces added
    allow spaces in bot commands

--- bot_cog.py
+++ bot_cog.py
@@ -17,12 +17,12 @@ class Asf:
         self.url_base = "http://127.0.0.1:1242/Api/Command/"
         pass
         
-    def get (self, name, param=None):
+    def get (self, params):
         """API access"""
-        if (param != None):
-            url = "{url_base}{name}%20{param}".format(url_base = self.url_base, name = name, param = param)
-        else:
-            url = "{url_base}{name}".format(url_base = self.url_base, name = name)
+
+		url = "{url_base}{params}".format(url_base = self.url_base, params = params)
+
+
         
         print (url)
         
@@ -37,18 +37,10 @@ class Asf:
     #@commands.listen()
     async def on_message(self,message):
         if ( message.content[0] == "!" and message.author.id == "bot_master_here"):
-            args = message.content[1:].split(" ")
-            if args == []:
-                return
-                
-            name = args[0]
-            if len(args) > 1:
-                param = args[1]
-            else:
-                param = None
+            args = message.content[1:].replace(" ","%20")
                 
             await self.bot.send_typing(message.channel)
-            response = self.get(name, param)
+            response = self.get(name, args)
             try:
                 await self.bot.send_message(message.channel, response)
             except:
